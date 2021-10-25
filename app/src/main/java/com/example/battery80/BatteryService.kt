d30c1b72c73b8f953956587d15f497254b4b4bcd
mawxim <brrcd@ya.ru>
2021-10-25 13:23:40 +0300

    added foreground service

---
+++ app/src/main/java/com/example/battery80/BatteryService.kt
@@ -0,0 +1,59 @@
+package com.example.battery80
+
+import android.app.*
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.IBinder
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.ServiceCompat.stopForeground
+
+class BatteryService: Service() {
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        createNotificationChannel()
+
+        val launchIntent = packageManager.getLaunchIntentForPackage(packageName)
+        val stackBuilder: TaskStackBuilder = TaskStackBuilder.create(this)
+        stackBuilder.addNextIntentWithParentStack(launchIntent)
+
+
+        val pendingIntent: PendingIntent = stackBuilder.getPendingIntent(
+            0,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val notification: Notification = Notification.Builder(this, CHANNEL_ID)
+            .setContentTitle("title")
+            .setContentText("test")
+            .setSmallIcon(R.drawable.ic_battery_full)
+            .setContentIntent(pendingIntent)
+            .build()
+
+        startForeground(122, notification)
+        // on receiving stop service action intent
+        // from MainActivity onDestroy method
+        if (intent?.action == ACTION_STOP_SERVICE)
+        {
+            stopForeground(true)
+            stopSelf()
+        }
+        return START_NOT_STICKY
+    }
+
+    private fun createNotificationChannel() {
+        val notificationChannel = NotificationChannel(CHANNEL_ID, "name",
+        NotificationManager.IMPORTANCE_LOW)
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(notificationChannel)
+    }
+
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
+
+    companion object {
+        const val CHANNEL_ID = "channel id"
+        const val ACTION_STOP_SERVICE = "stop service"
+    }
+}
--- app/src/main/java/com/example/battery80/MainActivity.kt
+++ app/src/main/java/com/example/battery80/MainActivity.kt
@@ -5,16 +5,23 @@ import android.content.IntentFilter
 import android.os.BatteryManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
+import com.example.battery80.BatteryService.Companion.ACTION_STOP_SERVICE
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        registerReceiver(
-            BatteryBroadcastReceiver(),
-            IntentFilter(Intent.ACTION_BATTERY_CHANGED)
-        )
+        val intent = Intent(this, BatteryService::class.java)
+        startForegroundService(intent)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        val stopIntent = Intent(this, BatteryService::class.java)
+        stopIntent.action = ACTION_STOP_SERVICE
+        startForegroundService(stopIntent)
     }
 }
